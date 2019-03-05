1f4810fa6267c2a6cf37fabd431ad2b1b61b2a8c
DarekR88 <dradmclad@gmail.com>
2019-03-05 14:31:49 -0600

    html

--- server.js
+++ server.js
@@ -4,8 +4,13 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/survey", function (req, res) {
+    res.sendFile(path.join(__dirname, "survey.html"));
+});
 
 app.get("/api/friendsList", function (req, res) {
     return res.json(reservations);
